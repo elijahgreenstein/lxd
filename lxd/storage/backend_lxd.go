91677dfce13f4ffc99c24f11a1b4472d0dc29e49
Thomas Parrott <thomas.parrott@canonical.com>
2026-05-01 13:40:37 +0100

    lxd/backup/backup/config/utils: Fix nil pointer dereference in instance backup restore
    Both the user triggered and admin triggered instance backup restore
    mechanism could lead to a daemon crash by being fed partial backup
    metadata.
    
    Addresses CVE-2026-41684 (https://github.com/lxc/incus/security/advisories/GHSA-x5r6-jr56-89pv)
    
    Reported-by: https://7asecurity.com
    Signed-off-by: Stéphane Graber <stgraber@stgraber.org>
    (cherry picked from 5ceb17630b459137ade4d4f6aad3c2b764df220b)
    Signed-off-by: Thomas Parrott <thomas.parrott@canonical.com>
    License: Apache-2.0

--- lxd/storage/backend_lxd.go
+++ lxd/storage/backend_lxd.go
@@ -817,6 +817,14 @@ func (b *lxdBackend) CreateInstance(inst instance.Instance, progressReporter iop
 // created in the database to run any storage layer finalisations, and a revert hook that can be
 // run if the instance database load process fails that will remove anything created thus far.
 func (b *lxdBackend) CreateInstanceFromBackup(srcBackup backup.Info, srcData io.ReadSeeker, progressReporter ioprogress.ProgressReporter) (func(instance.Instance) error, revert.Hook, error) {
+	if srcBackup.Config == nil {
+		return nil, nil, errors.New("Backup config is missing")
+	}
+
+	if srcBackup.Config.Instance == nil {
+		return nil, nil, errors.New("Instance definition in backup config is missing")
+	}
+
 	l := b.logger.AddContext(logger.Ctx{"project": srcBackup.Project, "instance": srcBackup.Name, "snapshots": srcBackup.Snapshots, "optimizedStorage": *srcBackup.OptimizedStorage})
 	l.Debug("CreateInstanceFromBackup started")
 	defer l.Debug("CreateInstanceFromBackup finished")
@@ -876,9 +884,6 @@ func (b *lxdBackend) CreateInstanceFromBackup(srcBackup backup.Info, srcData io.
 
 	// Check if the backup config contains a root volume and populate it for later use.
 	var rootVol *backupConfig.Volume
-	if srcBackup.Config == nil {
-		return nil, nil, errors.New("Backup config is missing")
-	}
 
 	// Returns an error if the backup doesn't contain a root volume.
 	rootVol, err = srcBackup.Config.RootVolume()
@@ -7034,6 +7039,14 @@ func (b *lxdBackend) UpdateInstanceBackupFile(inst instance.Instance, snapshots 
 // CheckInstanceBackupFileSnapshots compares the snapshots on the storage device to those defined in the backup
 // config supplied and returns an error if they do not match.
 func (b *lxdBackend) CheckInstanceBackupFileSnapshots(backupConf *backupConfig.Config, projectName string, progressReporter ioprogress.ProgressReporter) ([]*api.InstanceSnapshot, error) {
+	if backupConf == nil {
+		return nil, errors.New("Backup config is missing")
+	}
+
+	if backupConf.Instance == nil {
+		return nil, errors.New("Instance definition in backup config is missing")
+	}
+
 	l := b.logger.AddContext(logger.Ctx{"project": projectName, "instance": backupConf.Instance.Name})
 	l.Debug("CheckInstanceBackupFileSnapshots started")
 	defer l.Debug("CheckInstanceBackupFileSnapshots finished")
