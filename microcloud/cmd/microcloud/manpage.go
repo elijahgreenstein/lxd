49a76ad65f8d373472aad086bb2f6c33ccc65f54
Elijah Greenstein <elijah.greenstein@canonical.com>
2026-09-24 13:26:30 -0700

    doc: copy LXD manpage.go to MicroCloud code
    Signed-off-by: Elijah Greenstein <elijah.greenstein@canonical.com>

---
+++ microcloud/cmd/microcloud/manpage.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"github.com/spf13/cobra"
+	"github.com/spf13/cobra/doc"
+
+	"github.com/canonical/lxd/shared"
+	cli "github.com/canonical/lxd/shared/cmd"
+)
+
+type cmdManpage struct {
+	global *cmdGlobal
+
+	flagFormat string
+}
+
+func (c *cmdManpage) command() *cobra.Command {
+	cmd := &cobra.Command{}
+	cmd.Use = usage("manpage", "<target>")
+	cmd.Short = "Generate manpages for all commands"
+	cmd.Long = cli.FormatSection("Description", `Generate manpages for all commands`)
+	cmd.Hidden = true
+	cmd.Flags().StringVarP(&c.flagFormat, "format", "f", "man", cli.FormatStringFlagLabel("Format (man|md|rest|yaml)"))
+
+	cmd.RunE = c.run
+
+	return cmd
+}
+
+func (c *cmdManpage) run(cmd *cobra.Command, args []string) error {
+	// Quick checks.
+	exit, err := c.global.CheckArgs(cmd, args, 1, 1)
+	if exit {
+		return err
+	}
+
+	// If asked to do all commands, mark them all visible.
+	for _, c := range c.global.cmd.Commands() {
+		if c.Name() == "completion" {
+			continue
+		}
+
+		c.Hidden = false
+	}
+
+	// Generate the documentation.
+	switch c.flagFormat {
+	case "man":
+		header := &doc.GenManHeader{
+			Title:   "LXD - Command line client",
+			Section: "1",
+		}
+
+		opts := doc.GenManTreeOptions{
+			Header:           header,
+			Path:             shared.HostPathFollow(args[0]),
+			CommandSeparator: ".",
+		}
+
+		err = doc.GenManTreeFromOpts(c.global.cmd, opts)
+
+	case "md":
+		err = doc.GenMarkdownTree(c.global.cmd, shared.HostPathFollow(args[0]))
+
+	case "rest":
+		err = doc.GenReSTTree(c.global.cmd, shared.HostPathFollow(args[0]))
+
+	case "yaml":
+		err = doc.GenYamlTree(c.global.cmd, shared.HostPathFollow(args[0]))
+	}
+
+	return err
+}
+
